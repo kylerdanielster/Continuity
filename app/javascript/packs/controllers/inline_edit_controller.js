ac6480bca93d6e01d5f2a80988884dc108acb694
kylerdanielster <kylerdanielster@gmail.com>
2018-08-16 16:01:59 -0500

    everythings working except hiding forms and udpated comment

--- app/javascript/packs/controllers/inline_edit_controller.js
+++ app/javascript/packs/controllers/inline_edit_controller.js
@@ -1,8 +1,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    static targets = ["body"]
+    static targets = ["body", "form"]
     initialize() {
         console.log('Inline edit form controller initialized')
+    }
+
+    toggle() {
+        this.bodyTarget.classList.add('d-none')
+        this.formTarget.classList.remove('d-none')
     }
 }
