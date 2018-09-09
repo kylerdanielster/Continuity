c7f7ae85cbe90c7b675fa4d96d46eeda5998a7f4
kylerdanielster <kylerdanielster@gmail.com>
2018-09-09 13:25:38 -0500

    moved chatroom to partial

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,6 @@ Rails.application.routes.draw do
   resources :accepted_answers, only: [:create], module: :question_threads
 
   resources :chatrooms do
-    resource :chatroom_users
     resources :messages
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
